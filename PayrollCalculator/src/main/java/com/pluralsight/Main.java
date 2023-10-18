9f96cda0dbbfd449ab04f1759cd60cca9f526871
Tyson Hall <tn8938@gmail.com>
2023-10-18 16:01:18 -0400

    Added comments to debugged code

--- PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -12,15 +12,15 @@ public class Main {
             FileReader fileReader = new FileReader("employees.csv");
             BufferedReader buffReader = new BufferedReader(fileReader);
             String input;
-            buffReader.readLine();
-            while((input = buffReader.readLine()) != null) {
+            buffReader.readLine(); // reads the current line & moves on to the next line
+            while((input = buffReader.readLine()) != null) { // while loop starts from line 2 of employees.csv
                 System.out.println(input);
                 String[] words = (input.toString()).split("\\|");
                 Employee e = new Employee(Integer.parseInt(words[0]),
                         words[1], Double.parseDouble(words[2]),
                         Double.parseDouble(words[3]));
                 System.out.printf(
-                        "Employee ID: %d, Employee Name: %s, Employee Pay: %f \n",
+                        "EmployeeID: %d, EmployeeName: %s, EmployeePay: %f \n",
                         e.getEmployeeId(), e.getName(), e.getCrossPay()
                 );
             }
